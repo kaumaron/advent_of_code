e25f9367cdd54a0e50ca43a3e2db04e2d8750697
Andrew K DeCotiis-Mauro <andrewkmauro@gmail.com>
2023-12-13 18:14:24 -0500

    partially working

--- gondola_lift.py
+++ gondola_lift.py
@@ -1,6 +1,56 @@
 import argparse
 import re
 
+not_symbols = '0123456789.'
+numbers = '0123456789'
+
+def symbol_mapper(arr_txt):
+    symbol_map = {}
+    for r, row in enumerate(s): 
+         for c, el in enumerate(row): 
+             if el not in not_symbols: 
+                 symbol_map[(r,c)] = el
+    return list(symbol_map.keys())
+
+
+def compute_neighbors(r,c, br, bc):
+    neighbors = []
+    for x in [-1, 0, 1]:
+        for y in [-1, 0, 1]:
+            neighbors.append((r+x, c+y))
+    return list(filter(lambda x: x[0] <= br and x[1] <= bc and x[0] >= 0 and x[1] >= 0, neighbors))
+
+
+def return_parts(r, c):
+    parts = []
+    # traverse right
+    print(f'traversing: {r[c:]}')
+    right = search_numbers(r[c:])
+    # traverse left
+    print(f'traversing: {r[:c+1][::-1]}')
+    left = search_numbers(r[:c+1][::-1])
+    if len(right) > 0:
+        parts.append(int(right))
+    if len(left) > 0:
+        parts.append(int(left[::-1]))
+    return parts
+
+def search_numbers(r):
+    for idx in range(len(r)):
+        if r[idx] in numbers:
+            continue
+        elif r[:idx]:
+            return r[:idx]
+        else:
+            return ''
+        
+def check_neighbors(r, c, bc):
+    numbers = {}
+    if r[c] in numbers:
+        numbers[c] = r[c]
+    else:
+        return None
+    
 
 
 if __name__ == '__main__':
@@ -11,8 +61,8 @@ if __name__ == '__main__':
 
     # Parser for command line args
     parser = argparse.ArgumentParser(
-                        prog='Trebuchet',
-                        description='Sums values of calibration data for a trebuchet',
+                        prog='Gondola Lift',
+                        description='Engine Part Numbers',
                         epilog='That is it.')
     parser.add_argument('-t', '--test',
                         action='store_true')
@@ -32,61 +82,49 @@ if __name__ == '__main__':
         file = input_path + filebase + file_ext
 
     if test:
-        true_total = 0
-        test_total = 0
-        
+        answer_list = []
+        test_list = []
         if mode == 1:
             with open(file=file) as f:
-                r,g,b = max_colors(f.readline())
+                answer_list = f.readline().split(',')
+                answer_sum = sum([int(val) for val in answer_list])
+                arr_input = []
                 for line in f.readlines():
-                    pass
+                    arr_input.append(line)
+                bounds = (len(arr_input)-1, len(arr_input[0])-1)
 
-                    # true_value = 
-                    # test_value = 
 
-                    # true_total += true_value
-                    # test_total += test_value
-
-                    # if test_value != true_value:
-                    #     print(f'{line[0]} failed! {test_value} returned')
+                    
         if mode == 2:
             with open(file=file) as f:
-                r,g,b = max_colors(f.readline())
+                answer_list = f.readline().split(',')
+                answer_sum = sum([int(val) for val in answer_list])
                 for line in f.readlines():
                     pass
 
-                    # true_value = 
-                    # test_value = 
-
-                    # true_total += true_value
-                    # test_total += test_value
-
-                    # if test_value != true_value:
-                    #     print(f'{line[0]} failed! {test_value} returned')
 
         # for all tests
-        if true_total == test_total:
-            print('Total Value: Passed!')
+        test_sum = sum([int(val) for val in test_list])
+        if answer_list == test_list:
+            print('Part List: Passed!')
         else:
-            print(f'Total Value: Failed! {true_total} expected, {test_total} generated.')
+            print(f'Part List: Failed!\n{answer_list} expected,\n{test_list} generated.')
         quit() 
 
     if mode == 1:
-        true_total = 0
+        answer_list = 0
         with open(file=file) as f:
-                r,g,b = max_colors(f.readline())
                 for line in f.readlines():
                     pass
                     # true_total += game_possible(line[0], r, g, b)
-        print(true_total)
+        print(answer_list)
         quit()
     
     if mode == 2:
-        true_total = 0
+        answer_list = 0
         with open(file=file) as f:
-                r,g,b = max_colors(f.readline())
                 for line in f.readlines():
                     pass
                     # true_total += game_power(line[0])
-        print(true_total)
+        print(answer_list)
         quit()
